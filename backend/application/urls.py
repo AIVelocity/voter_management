3b0a179146c274a4c4e93389d68d10b9557b2c92
Nikhil Kanojiya <Nikhil Kanojiya@ho.rnt.ai>
2025-12-09 11:30:07 +0530

    feat:filter api

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -3,31 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
-    # voter list
-    path("voters/",views.voters_info,name="voters_info"),
-    # single voter info
-    path("voters/<int:voter_list_id>/", views.single_voters_info,name="single_voters_info"),
-    # voter addition
-    path("voter_add/",views.add_voter,name="add_voter"),
-    # voter update
-    path("voter_update/<int:voter_list_id>/", views.update_voter, name="update_voter"),
-    # tags
-    path("tags/",views.tags,name="tags"),
-    # voter search api
-    path("voters/search/", views.voters_search, name="voters_search"),
-    # voter family search
-    path("voters/family_search/",views.family_dropdown_search, name = "family_dropdown_search"),
-    # roles
-    path("roles/",views.roles,name="roles"),
-    # voter relation add
-    path("voters/relation_add/",views.add_relation,name="add_relation"),
-    # voter relation remove
-    path("voters/relation_remove/",views.remove_relation,name="remove_relation"),
-    # dashboard 
-    path("admin/dashboard/",views.dashboard,name="dashboard"),
-    # filter 
-    path("voters/filter/",views.filter,name="filter")
+    path("",views.index,name="index"),   
+    path("voters/",views.voters_info,name="voters_info"),# voter list   
+    path("voters/<int:voter_list_id>/", views.single_voters_info,name="single_voters_info"),# single voter info
+    path("voter_add/",views.add_voter,name="add_voter"),# voter addition    
+    path("voter_update/<int:voter_list_id>/", views.update_voter, name="update_voter"),# voter update   
+    path("tags/",views.tags,name="tags"),# tags   
+    path("voters/search/", views.voters_search, name="voters_search"),# voter search api    
+    path("voters/family_search/",views.family_dropdown_search, name = "family_dropdown_search"),# voter family search  
+    path("roles/",views.roles,name="roles"),# roles 
+    path("voters/relation_add/",views.add_relation,name="add_relation"),# voter relation add 
+    path("voters/relation_remove/",views.remove_relation,name="remove_relation"),# voter relation remove
+    path("admin/dashboard/",views.dashboard,name="dashboard"),# dashboard 
+    path("voters/filter/",views.filter,name="filter")# filter 
 ]
 
 # POST /api/voter/relation/add
