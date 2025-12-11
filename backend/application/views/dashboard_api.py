322fbd342f7a19819cdfcb12333100bc5e1718f8
Nikhil Kanojiya <Nikhil Kanojiya@ho.rnt.ai>
2025-12-11 21:14:13 +0530

    chore: modified models and occupation

--- backend/application/views/dashboard_api.py
+++ backend/application/views/dashboard_api.py
@@ -9,6 +9,13 @@ def dashboard(request):
     
     total_voters = VoterList.objects.count()
     # voter_list_count = VoterList.objects.filter(vo)
+    golden_color_tags = VoterList.objects.filter(tag_id = 4)
+    red_color_tags = VoterList.objects.filter(tag_id = 3)
+    orange_color_tags = VoterList.objects.filter(tag_id = 2)
+    green_color_tags = VoterList.objects.filter(tag_id = 1)
+    
+    
+    
     return JsonResponse({
         "total_voters" : total_voters
     })
