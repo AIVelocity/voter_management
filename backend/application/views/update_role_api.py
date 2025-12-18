06c935efb8446c6f447b25b693e06bec0c19effb
Nikhil Kanojiya <Nikhil Kanojiya@ho.rnt.ai>
2025-12-18 11:29:49 +0530

    chore: modified settings py

--- backend/application/views/update_role_api.py
+++ backend/application/views/update_role_api.py
@@ -32,7 +32,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-@api_view(["POST"])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def single_volunteer(request, user_id):
 
