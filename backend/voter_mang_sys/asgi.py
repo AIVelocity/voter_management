e16459109b2e225eaafa9d5a058617d140a37c97
AMIT KUMAR RAI <118451363+amitKrai22@users.noreply.github.com>
2025-12-29 16:54:02 +0530

    chore: add websocket authentication

--- backend/voter_mang_sys/asgi.py
+++ backend/voter_mang_sys/asgi.py
@@ -2,11 +2,11 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from notifications.routing import websocket_urlpatterns
-from voter_mang_sys.channel_jwt_middleware import JWTAuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voter_mang_sys.settings")
 django.setup()
+from notifications.routing import websocket_urlpatterns
+from voter_mang_sys.channel_jwt_middleware import JWTAuthMiddlewareStack
 django_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
