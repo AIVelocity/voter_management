73186040d5a59ec27b3cda7bafd6ecbc0c99dec8
Nikhil Kanojiya <Nikhil Kanojiya@ho.rnt.ai>
2026-01-02 15:00:46 +0530

    feat: added rate limiter

--- backend/application/views/rate_limiter.py
+++ backend/application/views/rate_limiter.py
@@ -4,15 +4,15 @@ class LoginRateThrottle(SimpleRateThrottle):
     scope = "login"
 
     def get_cache_key(self, request, view):
-        # Rate limit per mobile number OR IP
+        ip = self.get_ident(request)
         mobile = request.data.get("mobile_no")
-        if mobile:
-            return self.cache_format % {
-                "scope": self.scope,
-                "ident": mobile
-            }
+
+        if not mobile:
+            ident = ip
+        else:
+            ident = f"{ip}:{mobile}"
 
         return self.cache_format % {
             "scope": self.scope,
-            "ident": self.get_ident(request)
+            "ident": ident
         }
